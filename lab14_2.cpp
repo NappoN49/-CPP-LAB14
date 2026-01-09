8386878cd76f9f822dc43f04e7b8e4623fa3b724
NappoN49 <parawin39228@gmail.com>
2026-01-09 15:50:56 +0700

    lab14_2 complete

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,30 @@ int main(){
 }
 
 //Do not modify source code above this line 
+void showMatrix(const bool X[N][N]){
+	for(int i=0;i<N;i++){
+		for(int j=0;j<N;j++) cout<<X[i][j]<<' ';
+		cout<<"\n";
+	}
+}
+
+void inputMatrix(double X[N][N]){
+	for(int i=0;i<N;i++){
+		cout<<"Row "<<i+1<<": ";
+		for(int j=0;j<N;j++){
+			cin>>X[i][j];
+		}
+	}
+}
+
+void findLocalMax(const double X[N][N], bool ans[N][N]){
+	for(int i=0;i<N;i++){
+		for(int j=0;j<N;j++){
+			if(i==0 || j==0 || i==N-1 || j==N-1) ans[i][j]=0;
+			else{
+				if(X[i][j]>=X[i-1][j] && X[i][j]>=X[i][j-1] && X[i][j]>=X[i+1][j] && X[i][j]>=X[i][j+1]) ans[i][j]=1;
+				else ans[i][j]=0;
+			}
+		}
+	}
+}
