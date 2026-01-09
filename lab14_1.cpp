a6e5082366a708ed687953271c662697687a0fe1
NappoN49 <parawin39228@gmail.com>
2026-01-09 15:32:29 +0700

    lab14_1 complete

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -3,7 +3,15 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+	for(int i=1;i<N;i++){
+		T temp=d[i];
+		int j;
+		for(j=i-1;j>=0&&d[j]<temp;j--) d[j+1]=d[j];
+		d[j+1]=temp;
+		cout<<"Pass "<<i<<":";
+		for(int j=0;j<N;j++) cout<<d[j]<<' ';
+		cout<<"\n";
+	}
 }
 
 int main(){
